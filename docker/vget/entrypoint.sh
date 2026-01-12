1b82e8751cfe3680de15417ee994ded73d9b845a
Yumin Gui <guiyumin@protonmail.com>
2026-01-12 01:12:19 -0800

    fix: update ownership commands in entrypoint script for consistency

--- docker/vget/entrypoint.sh
+++ docker/vget/entrypoint.sh
@@ -27,8 +27,8 @@ fi
 
 # Fix ownership of mounted volumes if running as root
 if [ "$(id -u)" = "0" ]; then
-    chown -R vget:vget /home/vget/downloads /home/vget/.config/vget
-    exec gosu vget vget-server "$@"
+    chown -R 1000:1000 /home/vget/downloads /home/vget/.config/vget
+    exec gosu 1000:1000 vget-server "$@"
 else
     exec vget-server "$@"
 fi
